90380b6393cbf7b3ae3db367d0279f193eee008c
Jakob Merz <jakob.merz@whu.edu>
2019-05-12 17:06:22 +0200

    excercise 1 data types

--- data-types.py
+++ data-types.py
@@ -29,3 +29,45 @@ def f(x):
         return 'negative'
     return 'nonnegative'
 f(2)
+
+# Try out docstring
+def f(x):
+    """
+    This function squares its argument
+    """
+    return x**2
+
+f?
+f??
+
+# lamda for simple one line functions
+f = lamda x: x**3
+
+# example to calculate integral
+from scipy.integrate import quad
+
+quad(lambda x: x**3, 0, 2)
+
+# Excercises Data types:
+
+# Excercise 1 Part 1:
+x_vals = [2,3,4,5,6,7,8]
+y_vals = [20,31,14,25,26,57,78]
+
+for x_val, y_val in zip(x_vals, y_vals):
+    print(x_val * y_val)
+
+# Excercise 1 Part 2:
+x = 0
+for i in list(range(100)):
+    if i % 2 == 0:
+        x += 1
+print(x)
+
+# Excercise 1 Part 3:
+x = 0
+pairs = ((2, 5), (4, 2), (9, 8), (12, 10))
+for pair in pairs:
+    if pair[0] % 2 == 0 and pair [1] % 2 == 0:
+        x += 1
+print(x)
