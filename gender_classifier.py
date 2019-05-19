d904e02d933837131dfbec6bb6274bcaba99cb6a
Jakob Merz <jakob.merz@whu.edu>
2019-05-19 18:15:43 +0200

    fix mismatch between entries

--- gender_classifier.py
+++ gender_classifier.py
@@ -3,10 +3,11 @@ from sklearn import tree
 # [height,weight, shoe size]
 X = [[180,90,44], [140,50,37],[150,60,39],[180,90,46],[130,40,35],[140,60,40],[170,85,42],[130,50,34],[160,75,39],[190,110,48]]
 
-Y = ['male', 'female', 'female', 'male', 'female', 'female', 'male', 'male', 'male']
+Y = ['male', 'female', 'female', 'male', 'female', 'female', 'male', 'female', 'male', 'male']
 
-classifier = tree.DecisiontreeClassifier()
-classifier = classifier.fit(X,Y)
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X,Y)
 
-prediction = classifier.predict([[190,80,48]])
-print prediction
+prediction = clf.predict([[190,80,48]])
+print(prediction)
+
